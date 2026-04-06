f7a4b5bab5be11b0b51505d87a47c9b196611fe9
db5808 <db5808@srmist.edu.in>
2026-04-06 13:50:46 +0530

    Update routing.py

--- data-ai/utils/routing.py
+++ data-ai/utils/routing.py
@@ -4,11 +4,7 @@ BLOCKED_ROADS = ["OMR Phase 1", "Arcot Road", "Velachery Main Road"]
 
 def get_smart_route(start_lat, start_lng, end_lat, end_lng):
     try:
-<<<<<<< HEAD
-        url = "http://router.project-osrm.org/route/v1/driving/" + str(start_lng) + "," + str(start_lat) + ";" + str(end_lng) + "," + str(end_lat)
-=======
         url = f"http://router.project-osrm.org/route/v1/driving/{start_lng},{start_lat};{end_lng},{end_lat}"
->>>>>>> b5969d478d9be1b63c90fb9d8edca3f246b56a79
         params = {"overview": "full", "geometries": "geojson", "steps": "true"}
         response = requests.get(url, params=params, timeout=5)
         data = response.json()
@@ -22,8 +18,4 @@ def get_smart_route(start_lat, start_lng, end_lat, end_lng):
             "note": "Route calculated avoiding current construction and flood zones"
         }
     except Exception as e:
-<<<<<<< HEAD
         return {"status": "error", "message": str(e)}
-=======
-        return {"status": "error", "message": str(e)}
->>>>>>> b5969d478d9be1b63c90fb9d8edca3f246b56a79
